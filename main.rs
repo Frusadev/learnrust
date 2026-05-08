ca0311f015d952e7bb9556f3412a4214117cead3
Frusadev <frusadev@gmail.com>
2026-05-08 11:34:35 +0000

    feat: collatz sequence

--- main.rs
+++ main.rs
@@ -7,10 +7,25 @@ fn finbonacci(n: u64) -> u64 {
 }
 
 fn collatz_length(n: u64) -> u64 {
-
+    let mut current_length: u64 = 1;
+    let mut current_val: u64 = n;
+    if n == 0 {
+        return 0;
+    }
+    while current_val != 1 {
+        if current_val % 2 == 0 {
+            current_val /= 2
+        } else {
+            current_val = 3 * current_val + 1;
+        }
+        current_length += 1;
+    }
+    current_length
 }
 
 fn main() {
     let fib = finbonacci(12);
-    println!("Fib: {fib}")
+    println!("Fib: {fib}");
+    let cl = collatz_length(11);
+    println!("Collatz Length: {cl}")
 }
