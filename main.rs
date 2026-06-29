b385f72cb4282d00bf69ef7621b0f1dec2e4e686
Frusadev <frusadev@gmail.com>
2026-06-29 15:30:16 +0000

    enum, structs

--- main.rs
+++ main.rs
@@ -1,90 +1,68 @@
-fn finbonacci(n: u64) -> u64 {
-    if n < 2 {
-        return n;
-    } else {
-        return finbonacci(n - 1) + finbonacci(n - 2);
-    }
+enum TaskState {
+    Running,
+    Stopped,
+    Failed(String),
 }
 
-fn collatz_length(n: u64) -> u64 {
-    let mut current_length: u64 = 1;
-    let mut current_val: u64 = n;
-    if n == 0 {
-        return 0;
-    }
-    while current_val != 1 {
-        if current_val % 2 == 0 {
-            current_val /= 2
-        } else {
-            current_val = 3 * current_val + 1;
-        }
-        current_length += 1;
-    }
-    current_length
+struct Task {
+    id: u32,
+    state: TaskState,
 }
 
-fn transpose(m: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-    let mut final_matrix: [[i32; 3]; 3] = [
-        [0, 0, 0], // lol
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
-    for i in 0..3 {
-        for x in 0..3 {
-            final_matrix[i][x] = m[x][i];
+impl Task {
+    fn restart(&mut self) {
+        match self.state {
+            TaskState::Failed(_) => {
+                self.state = TaskState::Running;
+                println!("Task {} started", &self.id);
+            }
+            TaskState::Stopped => {
+                self.state = TaskState::Running;
+                println!("Task {} started", &self.id);
+            }
+            _ => {
+                println!("Task {} is already running", &self.id)
+            }
         }
     }
-    final_matrix
 }
 
-fn magnitude(vec3: &[f64; 3]) -> f64 {
-    let mut s: f64 = 0.0;
-    for p in vec3 {
-        s += p * p
-    }
-    s.sqrt()
+fn print_state(state: &TaskState) {
+    let text = match state {
+        TaskState::Running => String::from("The task is running"),
+        TaskState::Stopped => String::from("The task is not running"),
+        TaskState::Failed(s) => format!("The task failed to start due to {}", s),
+    };
+    println!("{}", text);
 }
 
-fn normalize(vector: &mut [f64; 3]) {
-    let mg = magnitude(vector);
-    for item in vector {
-        *item /= mg;
+fn is_running(state: &TaskState) -> bool {
+    let result: bool;
+    match state {
+        TaskState::Running => result = true,
+        _ => result = false,
     }
+    return result;
 }
 
 fn main() {
-    let fib = finbonacci(12);
-    println!("Fib: {fib}");
-    let cl = collatz_length(11);
-    println!("Collatz Length: {cl}");
-    let matrix_to_transpose = [
-        [101, 102, 103], // <-- the comment makes rustfmt add a newline
-        [201, 202, 203],
-        [301, 302, 303],
-    ];
-    println!("Original:");
-    for row in matrix_to_transpose {
-        println!("{row:?}")
-    }
-    let transposed = transpose(matrix_to_transpose);
-    println!("Transposed: ");
-    for row in transposed {
-        println!("{row:?}")
-    }
+    let running_task = TaskState::Running;
+    let stopped_task = TaskState::Stopped;
+    let failed_task = TaskState::Failed(String::from("A specific reason"));
 
-    let arr = [1, 2, 3, 4, 5];
-    let s: &[i32] = &arr[1..3];
+    print_state(&running_task);
+    print_state(&stopped_task);
+    print_state(&failed_task);
 
-    println!("\nSlice test");
-    println!("{s:?}");
+    println!("Is running? : {}", is_running(&running_task));
+    println!("Is running? : {}", is_running(&stopped_task));
+    println!("Is running? : {}", is_running(&failed_task));
 
-    println!(
-        "Magnitude of a unit vector: {}",
-        magnitude(&[0.0, 1.0, 0.0])
-    );
+    let mut task = Task {
+        id: 1,
+        state: failed_task,
+    };
 
-    let mut v = [1.0, 2.0, 9.0];
-    println!("Magnitude of {v:?}: {}", magnitude(&v));
-    normalize(&mut v);
-    println!("Magnitude of {v:?} after normalization: {}", magnitude(&v));
+    task.restart();
 }
+
