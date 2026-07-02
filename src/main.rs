1816f43baf505b142cd2b76aaff274f6cd037c24
Frusadev <frusadev@gmail.com>
2026-07-02 17:28:11 +0000

    fresh

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use crate::math;
-
 fn main() {
-    let numbers: Vec<i32> = vec![1, 2, 3, 4, 4, 5, 6];
-    let result: Vec<i32> = numbers
-        .iter()
-        .filter(|x| *x % 2 == 0)
-        .map(|x| x * x)
-        .collect();
-    for n in &numbers {
-        println!("{n}")
-    }
-    println!("------------------------------------------");
-    for n in &result {
-        println!("{n}")
-    }
+    println!("Hello, world!");
 }
--- src/math.rs
+++
@@ -1,5 +0,0 @@
-mod math;
-
-pub fn add(lhs: &i32, rhs: &i32) {
-    return *lhs + *rhs;
-}
