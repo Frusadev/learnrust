314f5d0a83286c9a7f6eece0dc6fb46595baccf5
Frusadev <frusadev@gmail.com>
2026-07-03 19:14:28 +0000

    tokio !

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() { x } else { y }
-}
+use std::time;
 
-fn main() {
-    let long = longest("This is daniel!!!", "Hello world!");
-    println!("{long}")
+#[tokio::main]
+async fn main() {
+    println!("Start...");
+
+    tokio::spawn(async {
+        for i in 1..=5 {
+            println!("Task A: {i}")
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    });
+
+    for i in 1..=5 {
+        println!("Task B: {i}");
+        tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+    }
+
+    tokio::time::sleep(time::Duration::from_secs(3)).await;
 }
